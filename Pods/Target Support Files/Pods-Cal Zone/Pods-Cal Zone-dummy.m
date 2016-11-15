8ed266d7c4191f25f8da478fd15ea087abf9cd8f
Rahul Maddineni <rahulmaddineni@Rahuls-Mac.local>
2016-11-15 17:28:54 -0500

    Pods config

---
+++ Pods/Target Support Files/Pods-Cal Zone/Pods-Cal Zone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cal_Zone : NSObject
+@end
+@implementation PodsDummy_Pods_Cal_Zone
+@end
---
+++ Pods/Target Support Files/QuadratTouch/QuadratTouch-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "QuadratTouch.h"
+
+FOUNDATION_EXPORT double QuadratTouchVersionNumber;
+FOUNDATION_EXPORT const unsigned char QuadratTouchVersionString[];
+
